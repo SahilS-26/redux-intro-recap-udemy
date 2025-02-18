48bb8a9268322c76af3e308ce634d84c1223e113
Sahil Suthar <sahil26dd@gmail.com>
2025-02-19 00:04:04 +0530

    lect 272: the redux devTools

--- src/features/customers/customerSlice.js
+++ src/features/customers/customerSlice.js
@@ -13,7 +13,7 @@ export default function customerReducer(state = initialStateCustomer, action) {
         nationalID: action.payload.nationalID,
         createdAt: action.payload.createdAt,
       };
-    case "customer/updateCustomer":
+    case "customer/updateName":
       return {
         ...state,
         fullName: action.payload,
@@ -33,7 +33,7 @@ export function createCustomer(fullName, nationalID) {
 
 export function updateCustomer(fullName) {
   return {
-    type: "customer/updateCustomer",
+    type: "customer/updateName",
     payload: fullName,
   };
 }
