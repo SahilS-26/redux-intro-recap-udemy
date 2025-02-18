88eb0588bad94453f7c93436f92e40c3004aee88
Sahil Suthar <sahil26dd@gmail.com>
2025-02-18 23:39:52 +0530

    lect 271: making an API call with redux thunk

--- src/features/accounts/accountSlice.js
+++ src/features/accounts/accountSlice.js
@@ -2,6 +2,7 @@ const initialStateAccount = {
   balance: 0,
   loan: 0,
   loanPurpose: "",
+  isLoading: false,
 };
 
 export default function accountReducer(state = initialStateAccount, action) {
@@ -10,6 +11,7 @@ export default function accountReducer(state = initialStateAccount, action) {
       return {
         ...state,
         balance: state.balance + action.payload,
+        isLoading: false,
       };
     case "account/withdraw":
       return {
@@ -32,14 +34,31 @@ export default function accountReducer(state = initialStateAccount, action) {
         loanPurpose: "",
         balance: state.balance - state.loan,
       };
+    case "account/convertingCurrency":
+      return { ...state, isLoading: true };
 
     default:
       return state;
   }
 }
 
-export function deposit(amount) {
-  return { type: "account/deposit", payload: amount };
+export function deposit(amount, currency) {
+  const TO_CURRENCY = "USD";
+  if (currency === "USD") return { type: "account/deposit", payload: amount };
+
+  return async function (dispatch, getState) {
+    dispatch({ type: "account/convertingCurrency" });
+    // API call
+    const res = await fetch(
+      `https://api.frankfurter.dev/v1/latest?base=${currency}&symbols=${TO_CURRENCY}`
+    );
+    const data = await res.json();
+
+    const convertedAmount = +(amount * data.rates[TO_CURRENCY]).toFixed(2);
+
+    // return action
+    dispatch({ type: "account/deposit", payload: convertedAmount });
+  };
 }
 export function withdraw(amount) {
   return { type: "account/withdraw", payload: amount };
